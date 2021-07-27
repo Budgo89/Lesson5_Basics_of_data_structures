2bb8315e1cbd13608b204490e1863c57eb528ec3
Budgo <Budgo@mail.ru>
2021-07-27 14:14:01 +0300

    Создание проекта

---
+++ Lesson5_Basics_of_data_structures/Lesson5_Basics_of_data_structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson5_Basics_of_data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
