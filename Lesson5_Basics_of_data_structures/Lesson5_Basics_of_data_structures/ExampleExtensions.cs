6617033443d0eca22efab862b482fb2dd0530d86
Budgo <Budgo@mail.ru>
2021-07-27 18:36:56 +0300

    ДЗ готово

---
+++ Lesson5_Basics_of_data_structures/Lesson5_Basics_of_data_structures/ExampleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_Basics_of_data_structures
+{
+    public static class ExampleExtensions
+    {
+        public static int CharCount(this string str)
+        {
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {               
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
